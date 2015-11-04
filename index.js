2b20d705255364353a5d6db7e65e9ae6f6c40023
Ryan Clark <ryan.clark.j@gmail.com>
2015-11-04 14:46:49 -0700

    allow caller to provide an s3 client

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 var AWS = require('aws-sdk');
-var s3 = new AWS.S3();
 var queue = require('queue-async');
 
 module.exports = cleanup;
 module.exports.listAll = listAll;
-module.exports.s3 = s3;
 
 /**
  * Cleanup stale S3 multipart uploads by aborting stale MPUs.
@@ -12,11 +10,13 @@ module.exports.s3 = s3;
  * @param {String} options.bucket S3 bucket to cleanup
  * @param {Date} options.before Oldest date of MPUs to leave untouched. Defaults to the current time - 24 hours
  * @param {Function} options.logger Optional function to call after each abort operation for logging
+ * @param {Object} options.s3 Optional s3 client to use
  * @param {Function} callback Callback function
  */
 function cleanup(options, callback) {
     options.before = options.before || new Date(+new Date() - 864e5);
     options.logger = options.logger || function() {};
+    var s3 = options.s3 || new AWS.S3();
     listAll(options, function(err, uploads) {
         if (err) return callback(err);
         var q = queue(4);
@@ -47,9 +47,11 @@ function cleanup(options, callback) {
  * List all MPUs for a bucket.
  * @param {Object} options
  * @param {String} options.bucket S3 bucket to list
+ * @param {Object} options.s3 Optional s3 client to use
  * @param {Function} callback Callback function
  */
 function listAll(options, callback) {
+    var s3 = options.s3 || new AWS.S3();
     var uploads = [];
     function ls(markers) {
         var params ={ Bucket: options.bucket };
@@ -69,4 +71,3 @@ function listAll(options, callback) {
     }
     ls();
 }
-
